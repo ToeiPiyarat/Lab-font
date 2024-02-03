36e240ebd04878c7feec1710009a5b6bc50d6f1f
ToeiPiyarat <toeisopamine23@gmail.com>
2024-02-03 21:02:18 +0700

    font

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
+import AppRouter from "./router/AppRouter";
+import useAuth from "./hooks/useAuth";
+
 function App() {
+  const {loading} = useAuth()
+
+  if(loading) {
+    return (
+      <p className="text-4xl text-primary">Loading...</p>
+    )
+  }
+
   return (
-    <div data-theme="" className="min-h-screen" >
-      <h1 className="text-4xl text-pink-600 text-center border bg-lime-400 py-5 m-6 font-bold">
-        Hello world!
-      </h1>
-      <input type="checkbox" value="synthwave" className="toggle theme-controller"/>
-      <div className="flex gap-3 border justify-center py-3">
-        <button className="btn">Button</button>
-        <button className="btn btn-neutral">Neutral</button>
-        <button className="btn btn-primary">Primary</button>
-        <button className="btn btn-secondary">Secondary</button>
-        <button className="btn btn-accent">Accent</button>
-        <button className="btn btn-ghost">Ghost</button>
-        <button className="btn btn-link">Link</button>
-      </div>
+    <div className="min-h-screen">
+    <AppRouter/>
     </div>
   );
 }
